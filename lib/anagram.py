60aeb6f3bcce532d7c5ee7749843552001a9de9c
Isaac Odiwuor Odhiambo <odhiamboisaach@gmail.com>
2024-09-01 14:40:17 +0300

    done

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, word_list):
+        # Helper function to check if two words are anagrams
+        def are_anagrams(word1, word2):
+            return sorted(word1) == sorted(word2)
+        
+        # Find and return words that are anagrams of self.word
+        return [word for word in word_list if are_anagrams(self.word, word)]
